bb89c0039a87afc67496ab24c74eaacd04a5f7c9
Pavel Šturc <psturc@redhat.com>
2018-11-27 12:57:22 +0100

    fix: update mobile client types

--- pkg/apis/mobile/v1alpha1/types.go
+++ pkg/apis/mobile/v1alpha1/types.go
@@ -1,6 +1,10 @@
 package v1alpha1
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	"encoding/json"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
@@ -8,16 +12,13 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 type MobileClientList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
-
-	Items []MobileClient `json:"items" protobuf:"bytes,2,rep,name=items"`
+	Items           []MobileClient `json:"items" protobuf:"bytes,2,rep,name=items"`
 }
 
 type MobileClientSpec struct {
-	ApiKey        string `json:"apiKey"`
-	AppIdentifier string `json:"appIdentifier"`
-	ClientType    string `json:"clientType"`
-	DmzUrl        string `json:"dmzUrl"`
-	Name          string `json:"name"`
+	ApiKey string `json:"apiKey"`
+	DmzUrl string `json:"dmzUrl"`
+	Name   string `json:"name,required"`
 }
 
 // +genclient
@@ -26,5 +27,21 @@ type MobileClientSpec struct {
 type MobileClient struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
-	Spec              MobileClientSpec `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
+	Spec              MobileClientSpec   `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
+	Status            MobileClientStatus `json:"status,omitempty"`
+}
+
+//for mobile-services.json
+type MobileClientStatus struct {
+	Services []MobileClientService `json:"services"`
+}
+
+type MobileClientService struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Type string `json:"type"`
+	Url  string `json:"url"`
+	//ideally we would like to use map[string]interface{} type here, but we can't as the generated code will complain that the interface{} is not `DeepCopy`-able.
+	Config  json.RawMessage `json:"config"`
+	Version string          `json:"version"`
 }
